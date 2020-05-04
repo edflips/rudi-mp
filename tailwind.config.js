67bd97d88a7e4b0b288e5d5d4cba66f11e161f00
Ed Phillips <ed@zapnito.com>
2020-05-04 14:09:10 +0100

    oops

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
-/*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
 module.exports = {
+  purge: ['./pages/**/*.vue', './components/**/*.vue', './layouts/**/*.vue'],
   theme: {},
   variants: {},
-  plugins: []
+  plugins: [],
 }
